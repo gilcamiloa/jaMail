ad289cfcbe56b83c17fb9b89267e12a09e0805c1
gilcamiloa <cgilv6@gmail.com>
2022-03-05 23:42:33 -0500

    jamail part 2

--- src/jaMail/Email.java
+++ src/jaMail/Email.java
@@ -20,7 +20,7 @@ public class Email {
 		
 	//Return department associated with first / last
 	this.department = setDepartment();
-	System.out.println("Department:" + this.department);
+	System.out.println("Department: " + this.department);
 	
 	//Call a method that returns a random password
 	this.password = randomPassword(defaultPasswordLength);
@@ -31,7 +31,7 @@ public class Email {
 	
 	private String setDepartment() {
 		
-		System.out.println("DEPARTMENT CODES \n1 for Sales \n2 for Development \n3 for Accounting \n0 for none /n Enter the department code:");
+		System.out.println("DEPARTMENT CODES \n1 for Sales \n2 for Development \n3 for Accounting \n0 for none \n\nEnter the department code:");
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) {return "sales";}
@@ -43,11 +43,14 @@ public class Email {
 //		Generate random PW
 	
 	private String randomPassword(int length) {
-		String passwordset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-";
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-";
 		char[] password = new char[length];
-		for (i = 0; i < length; i++) {
+		for (int i = 0; i < length; i++) {
 			int rand = (int) (Math.random() * passwordSet.length());
 			password[i] = passwordSet.charAt(rand);
+			System.out.println(rand);
+			System.out.println(passwordSet.charAt(rand));
+			
 		}
 		return new String(password);
 	}
